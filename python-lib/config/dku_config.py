1d19bd43c82a0b248c5d113b265ead92a9668c04
HENRI CHABERT <chabhenrib@gmail.com>
2020-07-24 12:23:07 +0200

    Separated config check and loading

--- python-lib/config/dku_config.py
+++ python-lib/config/dku_config.py
@@ -9,15 +9,19 @@ class DkuConfig(object):
     @utils.log_func(txt='config loading')
     def load(self):
         self._load_recipe_param()
+        self._check_params()
         utils.display_gpu_device()
 
     def _load_recipe_param(self):
         self.recipe_config = get_recipe_config()
         should_use_gpu = self.recipe_config.get('should_use_gpu', False)
         self.gpu_options = utils.load_gpu_options(
-            should_use_gpu, self.recipe_config['list_gpu'], self.recipe_config['gpu_allocation'])
+            should_use_gpu, self.recipe_config.get('list_gpu'), self.recipe_config.get('gpu_allocation'))
         self.n_gpu = self.gpu_options.get("n_gpu", 0)
         self.use_gpu = should_use_gpu and self.n_gpu > 1
+
+    def _check_params(self):
+        pass
 
     def get(self, key, default=None):
         return getattr(self, key, default)
