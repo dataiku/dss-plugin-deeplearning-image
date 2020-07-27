4bd3617bb0b26577634ea93c761fb392bf7c7644
HENRI CHABERT <chabhenrib@gmail.com>
2020-07-27 11:42:05 +0200

    Translated tensorboard 1.4 to tensorboard 2.2

--- web-app-templates/standard/tensorboard-web-app/app.js
+++ web-app-templates/standard/tensorboard-web-app/app.js
@@ -1,2 +1,5 @@
 // Retrieve iframe and set the src attribute
-$("#tensorboard-iframe").attr("src", getWebAppBackendUrl('tensorboard-endpoint'))
+$.getJSON(getWebAppBackendUrl('tensorboard-endpoint'), function({ tb_url }) {
+    console.log('Received data from backend', tb_url)
+    $("#tensorboard-iframe").attr("src", tb_url)
+});
