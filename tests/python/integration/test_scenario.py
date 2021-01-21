02a9173f8239a9f78ddcb97f6b2af40586481634
HENRI CHABERT <chabhenrib@gmail.com>
2021-01-21 17:10:46 +0100

    Added integration tests

---
+++ tests/python/integration/test_scenario.py
@@ -0,0 +1,19 @@
+import pytest
+import logging
+
+from dku_plugin_test_utils import dss_scenario
+
+
+pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
+
+
+test_kwargs = {
+    "user": "user1",
+    "project_key": "PLUGINTESTDEEPLEARNINGIMAGE",
+    "logger": logging.getLogger("dss-plugin-test.deeplearning-image.test_scenario"),
+}
+
+
+def test_run_tensorboard_start(user_clients):
+    test_kwargs["client"] = user_clients[test_kwargs["user"]]
+    dss_scenario.run(scenario_id="Webapp_-_Tensorboard", **test_kwargs)
