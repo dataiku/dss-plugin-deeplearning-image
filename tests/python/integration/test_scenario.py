60ea780a0503a0f6d21885d7bc3435949ff5f38d
HENRI CHABERT <chabhenrib@gmail.com>
2021-01-25 12:35:09 +0100

    Added extract recipe integration test

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -17,3 +17,8 @@ test_kwargs = {
 def test_run_tensorboard_start(user_clients):
     test_kwargs["client"] = user_clients[test_kwargs["user"]]
     dss_scenario.run(scenario_id="Webapp_-_Tensorboard", **test_kwargs)
+
+
+def test_extract_recipe(user_clients):
+    test_kwargs["client"] = user_clients[test_kwargs["user"]]
+    dss_scenario.run(scenario_id="Recipe_-_Extract_features", **test_kwargs)
