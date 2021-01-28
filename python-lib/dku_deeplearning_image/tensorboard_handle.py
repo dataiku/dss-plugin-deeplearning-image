5c46cea23b38301b9f47c67b12254ace129ebdd1
HenriChabert <henri@famille-chabert.com>
2021-01-28 16:50:20 +0100

    Changed way tensorboard is served

--- python-lib/dku_deeplearning_image/tensorboard_handle.py
+++ python-lib/dku_deeplearning_image/tensorboard_handle.py
@@ -1,50 +1,86 @@
 import dataiku
-import dku_deeplearning_image.constants as constants
-from dataikuapi.utils import DataikuException
-import os
-from werkzeug.serving import make_server
+from dataiku.customwebapp import get_webapp_config
+import tensorboard.default as tb_default
 import tensorboard as tb
+from tensorboard.backend import application
+import os
 import logging
+from argparse import ArgumentParser
+from tensorboard.plugins import base_plugin
+from dataikuapi.utils import DataikuException
 
-logger = logging.getLogger(__name__)
-
-
-class TensorboardCustomServer(tb.program.TensorBoardServer):
-    """
-        Start a Tensorboard in a different server. Hack inspired by this GitHub post :
-        https://stackoverflow.com/questions/61942505/add-tensorboard-server-to-flask-endpoint
-    """
-    def __init__(self, tensorboard_app, flags):
-        self.app = tensorboard_app
-        self.host = flags.host
-        self.srv = make_server(self.host, 0, self.app)
-
-    def get_port(self):
-        return self.srv.server_port
-
-    def serve_forever(self):
-        self.srv.serve_forever()
-
-    def get_url(self):
-        return "http://%s:%s" % (self.host, self.get_port())
-
-    def print_serving_message(self):
-        pass  # Werkzeug's `serving.run_simple` handles this
+from dku_deeplearning_image.constants import TENSORBOARD_LOGS
 
 
 def get_logdir(folder_id):
     folder = dataiku.Folder(folder_id)
     try:
         folder_path = folder.get_path()
-        return os.path.join(folder_path, constants.TENSORBOARD_LOGS)
+        return os.path.join(folder_path, TENSORBOARD_LOGS)
     except Exception as err:
         raise DataikuException('Folder with ID %s does not exist.' % str(folder_id))
 
 
-def start_server_and_return_url(folder_id, host="127.0.0.1"):
-    program = tb.program.TensorBoard(server_class=TensorboardCustomServer)
-    program.configure(
-        logdir=get_logdir(folder_id),
-        host=host
+def __get_logs_path():
+    return get_logdir(get_webapp_config().get('retrained_model_folder'))
+
+
+def __get_custom_assets_zip_provider():
+    path = os.path.join(os.path.dirname(tb.__file__), 'webfiles.zip')
+    if not os.path.exists(path):
+        logging.warning('webfiles.zip static assets not found: %s', path)
+        return None
+    return lambda: open(path, "rb")
+
+
+def init_flags(loader_list):
+    parser = ArgumentParser()
+    for loader in loader_list:
+        loader.define_flags(parser)
+    flags = parser.parse_args([])
+    return flags
+
+
+def make_plugin_loader(plugin_spec):
+    """Returns a plugin loader for the given plugin.
+    Args:
+      plugin_spec: A TBPlugin subclass, or a TBLoader instance or subclass.
+    Returns:
+      A TBLoader for the given plugin.
+    """
+    if isinstance(plugin_spec, base_plugin.TBLoader):
+        print("case1")
+        return plugin_spec
+    if isinstance(plugin_spec, type):
+        if issubclass(plugin_spec, base_plugin.TBLoader):
+            print("case2")
+            return plugin_spec()
+        if issubclass(plugin_spec, base_plugin.TBPlugin):
+            print("case3")
+            return base_plugin.BasicLoader(plugin_spec)
+    raise TypeError("Not a TBLoader or TBPlugin subclass: %r" % (plugin_spec,))
+
+
+def __get_tb_app(tensorboard_logs):
+    plugins_or_loaders = tb_default.get_plugins()
+    loaders = [make_plugin_loader(plugin_spec) for plugin_spec in plugins_or_loaders]
+    flags = init_flags(loaders)
+    flags.purge_orphaned_data = True
+    flags.reload_interval = 5.0
+    flags.logdir = tensorboard_logs
+    return application.standard_tensorboard_wsgi(
+        plugin_loaders=loaders,
+        assets_zip_provider=__get_custom_assets_zip_provider(),
+        flags=flags
     )
-    return program.launch()
+
+
+def tensorboard_wsgi_app(environ, start_response):
+    if environ['PATH_INFO'] == '/__ping':
+        status = '200 OK'
+        headers = [('Content-type', 'text/plain; charset=utf-8')]
+        start_response(status, headers)
+        return [b"200"]
+    else:
+        return __get_tb_app(__get_logs_path())(environ, start_response)
+
