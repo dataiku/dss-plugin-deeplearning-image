4be234dbc7a4904e6d57c724d11f5863be1a0a2d
HenriChabert <henri@famille-chabert.com>
2021-02-01 12:38:39 -0400

    Finished tests for downloading the model

--- tests/python/unit/test_dku_model.py
+++ tests/python/unit/test_dku_model.py
@@ -1,6 +1,7 @@
 import pytest
 from utils_objects import DkuModel, VirtualManagedFolder
 import os
+import dku_deeplearning_image.constants as constants
 
 
 def create_base_model(output_folder, model_choice):
@@ -18,12 +19,17 @@ def create_base_model(output_folder, model_choice):
 class TestDkuModel:
     def setup_class(self):
         PATH = 'model_resnet_unit_test'
+        INFO_FILENAME = 'model_info.json'
         MODEL_CHOICE = 'resnet::imagenet'
         if not os.path.exists(PATH):
             os.mkdir(PATH)
-        output_folder = VirtualManagedFolder(PATH)
-        self.new_model = create_base_model(output_folder, MODEL_CHOICE)
-        self.new_model.download_from_web()
+        model_folder = VirtualManagedFolder(PATH)
+        if not os.path.exists(os.path.join(PATH, INFO_FILENAME)):
+            new_model = create_base_model(model_folder, MODEL_CHOICE)
+            new_model.download_from_web()
+
+        self.dku_model = DkuModel(model_folder)
+        self.dku_model.load_model({}, constants.SCORING)
 
     def test_init(self):
-        return not not self.new_model
+        return not not self.dku_model
