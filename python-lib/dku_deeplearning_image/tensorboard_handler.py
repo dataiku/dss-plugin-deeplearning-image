c128fc3ffe30c7393153fc74608aa1d515c6e29d
HenriChabert <henri@famille-chabert.com>
2021-03-19 17:43:28 +0100

    Don't need to transform plugin anymore

--- python-lib/dku_deeplearning_image/tensorboard_handler.py
+++ python-lib/dku_deeplearning_image/tensorboard_handler.py
@@ -6,7 +6,6 @@ from tensorboard.backend import application
 import os
 import logging
 from argparse import ArgumentParser
-from tensorboard.plugins import base_plugin
 from dataikuapi.utils import DataikuException
 
 from dku_deeplearning_image.dku_constants import TENSORBOARD_LOGS
@@ -55,29 +54,14 @@ def init_flags(loader_list):
     return flags
 
 
-def make_plugin_loader(plugin_spec):
-    """Returns a plugin loader for the given plugin.
-    Args:
-      plugin_spec: A TBPlugin subclass, or a TBLoader instance or subclass.
-    Returns:
-      A TBLoader for the given plugin.
-    """
-    if issubclass(plugin_spec, base_plugin.TBLoader):
-        return plugin_spec()
-    if issubclass(plugin_spec, base_plugin.TBPlugin):
-        return base_plugin.BasicLoader(plugin_spec)
-    raise TypeError("Not a TBLoader or TBPlugin subclass: %r" % (plugin_spec,))
-
-
 def __get_tb_app(tensorboard_logs):
     plugins_or_loaders = tb_default.get_plugins()
-    loaders = [make_plugin_loader(plugin_spec) for plugin_spec in plugins_or_loaders]
-    flags = init_flags(loaders)
+    flags = init_flags(plugins_or_loaders)
     flags.purge_orphaned_data = True
     flags.reload_interval = 5.0
     flags.logdir = tensorboard_logs
     return application.standard_tensorboard_wsgi(
-        plugin_loaders=loaders,
+        plugin_loaders=plugins_or_loaders,
         assets_zip_provider=__get_custom_assets_zip_provider(),
         flags=flags
     )
