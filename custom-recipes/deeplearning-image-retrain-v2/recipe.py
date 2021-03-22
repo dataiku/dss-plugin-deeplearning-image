11db0d8c364684b55bbd98fc16128e900a704194
HenriChabert <henri@famille-chabert.com>
2021-03-22 16:16:50 +0100

    Fixed deepcopy pb

--- custom-recipes/deeplearning-image-retrain-v2/recipe.py
+++ custom-recipes/deeplearning-image-retrain-v2/recipe.py
@@ -28,8 +28,7 @@ def get_input_output():
 
 
 def save_output_model(output_folder, model):
-    output_model = model.deepcopy(folder=output_folder)
-    output_model.save_to_folder()
+    model.save_to_folder(new_folder=output_folder)
 
 
 @utils.log_func(txt='recipes')
