95353cfa437dd9fc4e5b5736ba3fe2a158a5ffda
HenriChabert <henri@famille-chabert.com>
2021-03-25 21:40:02 +0100

    Update model url

--- python-lib/dku_deeplearning_image/keras_applications.py
+++ python-lib/dku_deeplearning_image/keras_applications.py
@@ -29,8 +29,8 @@ APPLICATIONS = [{
         "input_shape": (299, 299, 3),
         "weights": {
             constants.IMAGENET: {
-                "top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels.h5",
-                "no_top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels_notop.h5",
+                "top": "https://storage.googleapis.com/tensorflow/keras-applications/xception/xception_weights_tf_dim_ordering_tf_kernels.h5",
+                "no_top": "https://storage.googleapis.com/tensorflow/keras-applications/xception/xception_weights_tf_dim_ordering_tf_kernels_notop.h5",
             }
         }
     },
@@ -43,8 +43,8 @@ APPLICATIONS = [{
         "input_shape": (299, 299, 3),
         "weights": {
             constants.IMAGENET: {
-                "top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels.h5",
-                "no_top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
+                "top": "https://storage.googleapis.com/tensorflow/keras-applications/inception_v3/inception_v3_weights_tf_dim_ordering_tf_kernels.h5",
+                "no_top": "https://storage.googleapis.com/tensorflow/keras-applications/inception_v3/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
             }
         }
     },
@@ -57,8 +57,8 @@ APPLICATIONS = [{
         "input_shape": (224, 224, 3),
         "weights": {
             constants.IMAGENET: {
-                "top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5",
-                "no_top": "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5"
+                "top": "https://storage.googleapis.com/tensorflow/keras-applications/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels.h5",
+                "no_top": "https://storage.googleapis.com/tensorflow/keras-applications/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5"
             }
         }
 }]
