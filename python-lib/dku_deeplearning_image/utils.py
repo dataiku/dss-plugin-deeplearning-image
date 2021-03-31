1c979efbab97b6cf6c71f8cc030cbed422198021
HenriChabert <henri@famille-chabert.com>
2021-03-31 21:25:48 +0200

    Changed info to debug

--- python-lib/dku_deeplearning_image/utils.py
+++ python-lib/dku_deeplearning_image/utils.py
@@ -124,17 +124,15 @@ def get_file_path(folder_path, file_name):
     return os.path.join(str(folder_path), str(file_name))
 
 
-def get_cached_file_from_folder(folder, file_path, verbose=False):
+def get_cached_file_from_folder(folder, file_path):
     filename = file_path.replace('/', '_')
     if not (os.path.exists(filename)):
         with folder.get_download_stream(file_path) as stream:
             with open(filename, 'wb') as f:
                 f.write(stream.read())
-                if verbose:
-                    logger.info(f"cached file {file_path}")
+                logger.debug(f"Cached file {file_path}")
     else:
-        if verbose:
-            logger.info(f"read from cache {file_path}")
+        logger.debug(f"Read from cache {file_path}")
     return filename
 
 
