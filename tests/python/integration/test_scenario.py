10b99241b9bb6b4ae51f038b5060a41ce8bf980d
HenriChabert <henri@famille-chabert.com>
2021-04-07 19:52:05 +0200

    Added tests for new models

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -67,3 +67,28 @@ def test_model_xception(user_dss_clients):
 def test_model_vgg16(user_dss_clients):
     """Tests a specific model"""
     add_integration_test(user_dss_clients, "Model_-_VGG16")
+
+
+def test_model_densenet(user_dss_clients):
+    """Tests a specific model"""
+    add_integration_test(user_dss_clients, "Model_-_Resnet")
+
+
+def test_model_mobilenet(user_dss_clients):
+    """Tests a specific model"""
+    add_integration_test(user_dss_clients, "Model_-_MobileNet")
+
+
+def test_model_inception_resnet(user_dss_clients):
+    """Tests a specific model"""
+    add_integration_test(user_dss_clients, "Model_-_InceptionResnet")
+
+
+def test_model_nasnet_large(user_dss_clients):
+    """Tests a specific model"""
+    add_integration_test(user_dss_clients, "Model_-_NasNetLarge")
+
+
+def test_model_nasnet_mobile(user_dss_clients):
+    """Tests a specific model"""
+    add_integration_test(user_dss_clients, "Model_-_NasNetMobile")
