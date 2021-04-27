df4bf19a061f9c872f9520d2e546077fa03b89ae
HenriChabert <henri@famille-chabert.com>
2021-04-27 15:31:20 +0200

    Removed first test

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -1,15 +1,2 @@
-# -*- coding: utf-8 -*-
-from dku_plugin_test_utils import dss_scenario
-
-TEST_PROJECT_KEY = "PLUGINTESTDEEPLEARNINGIMAGE"
-
-
-def add_integration_test(user_dss_clients, scenario_id):
-    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id=scenario_id)
-
-
 def test_1(user_dss_clients):
-    return True
-
-def test_2(user_dss_clients):
     return True
