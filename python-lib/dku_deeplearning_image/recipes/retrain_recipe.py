f65434c668f54fb9b89423df82071f60bb1420e3
HenriChabert <henri@famille-chabert.com>
2021-06-03 18:30:38 +0200

    fix 1 for data augm

--- python-lib/dku_deeplearning_image/recipes/retrain_recipe.py
+++ python-lib/dku_deeplearning_image/recipes/retrain_recipe.py
@@ -118,8 +118,8 @@ class RetrainRecipe(DkuRecipe):
 
     def _get_augmented_images(self, image, extra_images_gen):
         def _run_image_augm(im, augm_gen):
-            image_augm = np.tile(im, (self.config.data_augmentation, 1, 1, 1))
-            augmented_images = next(augm_gen.flow(image_augm, batch_size=self.config.data_augmentation))
+            image_augm = np.tile(im, (self.config.n_augmentation, 1, 1, 1))
+            augmented_images = next(augm_gen.flow(image_augm, batch_size=self.config.n_augmentation)).astype("int8")
             return [im] + augmented_images
 
         return tf.numpy_function(
--- python-lib/dku_deeplearning_image/utils.py
+++ python-lib/dku_deeplearning_image/utils.py
@@ -196,9 +196,9 @@ def format_predictions_output(predictions, errors, classify=False, labels_df=Non
 def apply_preprocess_image(tfds, input_shape, preprocessing, is_b64=False):
     def _apply_preprocess_image(image_path):
         return tf.numpy_function(
-            func=lambda x: tf.cast(preprocess_img(x, input_shape, preprocessing, is_b64), tf.float32),
+            func=lambda x: tf.cast(preprocess_img(x, input_shape, preprocessing, is_b64), tf.int8),
             inp=[image_path],
-            Tout=tf.float32)
+            Tout=tf.int8)
 
     def _convert_errors(images):
         return tf.numpy_function(
